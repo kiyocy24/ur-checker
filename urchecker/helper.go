82b2c7303474df9b92ff16f63cc633fb7bc91a33
kiyocy24 <kiyocy24@gmail.com>
2022-09-11 17:39:27 +0900

    basic impl

---
+++ urchecker/helper.go
@@ -0,0 +1,21 @@
+package urchecker
+
+import (
+	"io"
+	"net/http"
+)
+
+func httpRequest(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
